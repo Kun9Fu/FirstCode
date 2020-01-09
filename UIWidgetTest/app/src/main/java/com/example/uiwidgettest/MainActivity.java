fed8634a71dd8f3c95ec0f93ee8b8c02c9b78092
Shaozhuang.Peng <Shaozhuang.Peng@Lavaszx.com>
2020-01-09 11:39:18 +0800

    UIWidgetTest Training

--- UIWidgetTest/app/src/main/java/com/example/uiwidgettest/MainActivity.java
+++ UIWidgetTest/app/src/main/java/com/example/uiwidgettest/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.uiwidgettest;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
@@ -14,6 +18,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private EditText editText;
 
     private ImageView imageView;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +28,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button button=(Button)findViewById(R.id.button);
         editText=(EditText)findViewById(R.id.edit_text);
         imageView=(ImageView)findViewById(R.id.image_view);
+        progressBar=(ProgressBar)findViewById(R.id.progress_bar);
         button.setOnClickListener(this);
         }
 
@@ -31,13 +37,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         switch (v.getId()){
             case R.id.button:
-              //  String inputText=editText.getText().toString();
-              //  Toast.makeText(MainActivity.this,inputText,Toast.LENGTH_SHORT).show();
-                imageView.setImageResource(R.drawable.img_2);
+                ProgressDialog progressDialog=new ProgressDialog(MainActivity.this);
+                progressDialog.setTitle("This is progressDialog");
+                progressDialog.setMessage("Loading...");
+                progressDialog.setCancelable(true);
+                progressDialog.show();
                 break;
                 default:
-                    break;
-        }
+                    break; }
 
     }
     }
